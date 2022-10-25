52748a6912bc3999b21ed7b7c694125218c36897
aCute5 <costantini95dario@gmail.com>
2022-10-25 16:27:09 +0200

    dice and mail print added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,29 @@ let chiediMail = (prompt("Qual è la tua mail?"))
 // const dataMail = (Email[0, 2])
 if (Email.includes(chiediMail)) {
 	console.log("La mail è corretta");
+    document.querySelector(".mail").innerHTML = chiediMail 
 } else {
 	console.log('La mail non è corretta');
+    document.querySelector(".mail").innerHTML = "La mail non è presente nel database"
 }
+
+
+const rollDice = document.querySelector(".roll")
+const userNumber =  document.getElementById("usernumber").innerHTML
+const pcNumber = document.getElementById("pcnumber").innerHTML
+
+rollDice.addEventListener("click", function (){
+    const userNumber =  Math.floor(Math.random() * 6)
+    const pcNumber =  Math.floor(Math.random() * 6)
+    document.getElementById("usernumber").innerHTML = "Il tuo numero:" + userNumber;
+    document.getElementById("pcnumber").innerHTML =  "Il numero del PC:" + pcNumber ;
+    
+    
+    if (userNumber > pcNumber) {
+        document.querySelector(".result").innerHTML = "Hai vinto"
+    }
+   else{
+         document.querySelector(".result").innerHTML = "Hai perso"
+   }
+})
+    
